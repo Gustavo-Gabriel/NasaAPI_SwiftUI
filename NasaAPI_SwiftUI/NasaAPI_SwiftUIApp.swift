07f23d8efe86559ebb7c531e17289171691e06ba
Gustavo Anjos <gustavoanjos42@gmail.com>
2021-02-11 08:49:42 -0400

    Initial Commit

---
+++ NasaAPI_SwiftUI/NasaAPI_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasaAPI_SwiftUIApp.swift
+//  NasaAPI_SwiftUI
+//
+//  Created by Gustavo Anjos on 11/02/21.
+//
+
+import SwiftUI
+
+@main
+struct NasaAPI_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
